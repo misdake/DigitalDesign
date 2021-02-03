12b91036fc24d4350a16559d2db2f2e2ecd977b9
misdake <misdake@gmail.com>
2021-02-03 23:05:15 +0800

    add component element and css.

---
+++ src/ui/component/ComponentElement.ts
@@ -0,0 +1,34 @@
+import {customElement, html, LitElement, property} from "lit-element";
+import {Component} from "../../logic/Component";
+
+@customElement('component-element')
+export class ComponentElement extends LitElement {
+
+    @property()
+    private component: Component;
+
+    render() {
+        let inputPins = Object.values(this.component.inputPins);
+        let outputPins = Object.values(this.component.outputPins);
+
+        let width = 300; //TODO 从component获取
+        let height = 50 * Math.max(2, inputPins.length, outputPins.length); //TODO 从component获取
+
+        let inputs = inputPins.map(pin => html`<div class="pin input-pin">${pin.name}</div>`);
+        let outputs = outputPins.map(pin => html`<div class="pin output-pin">${pin.name}</div>`);
+
+        return html`
+            <div class="component" style="width: ${width}px; height: ${height}px;">
+                <div class="component-name">${this.component.name}</div>
+                <div class="component-type">${this.component.type}</div>
+                <div class="input-pin-list">${inputs}</div>
+                <div class="output-pin-list">${outputs}</div>
+            </div>
+        `;
+    }
+
+    createRenderRoot() {
+        return this;
+    }
+
+}
