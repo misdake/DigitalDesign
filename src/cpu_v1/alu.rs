06a8528d517514e63e26f99c73f94d97d63dbacb
misdake <misdake@gmail.com>
2023-04-01 11:08:26 +0800

    fix alu.

--- src/cpu_v1/alu.rs
+++ src/cpu_v1/alu.rs
@@ -25,25 +25,24 @@ impl CpuComponent for CpuAlu {
     type Output = CpuAluOutput;
     fn build(input: &Self::Input) -> Self::Output {
         let alu0_select = input.alu0_select.wires;
-        let alu0_reg0 = input.reg0_data & alu0_select[1 << Alu0Select::Reg0 as u8].expand();
-        let alu0_reg0_inv = !input.reg0_data & alu0_select[1 << Alu0Select::Reg0Inv as u8].expand();
-        let alu0_zero = Wires::parse_u8(0) & alu0_select[1 << Alu0Select::Zero as u8].expand();
-        let alu0_imm = input.imm & alu0_select[1 << Alu0Select::Imm as u8].expand();
+        let alu0_reg0 = input.reg0_data & alu0_select[Alu0Select::Reg0 as usize].expand();
+        let alu0_reg0_inv = !input.reg0_data & alu0_select[Alu0Select::Reg0Inv as usize].expand();
+        let alu0_zero = Wires::parse_u8(0) & alu0_select[Alu0Select::Zero as usize].expand();
+        let alu0_imm = input.imm & alu0_select[Alu0Select::Imm as usize].expand();
         let alu0 = (alu0_reg0 | alu0_reg0_inv) | (alu0_zero | alu0_imm);
 
         let alu1_select = input.alu1_select.wires;
-        let alu1_reg1 = input.reg1_data & alu1_select[1 << Alu1Select::Reg1 as u8].expand();
-        let alu1_neg_one =
-            Wires::parse_u8(15) & alu1_select[1 << Alu1Select::NegOne as u8].expand();
-        let alu1_zero = Wires::parse_u8(0) & alu1_select[1 << Alu1Select::Zero as u8].expand();
-        let alu1_one = Wires::parse_u8(1) & alu1_select[1 << Alu1Select::One as u8].expand();
+        let alu1_reg1 = input.reg1_data & alu1_select[Alu1Select::Reg1 as usize].expand();
+        let alu1_neg_one = Wires::parse_u8(15) & alu1_select[Alu1Select::NegOne as usize].expand();
+        let alu1_zero = Wires::parse_u8(0) & alu1_select[Alu1Select::Zero as usize].expand();
+        let alu1_one = Wires::parse_u8(1) & alu1_select[Alu1Select::One as usize].expand();
         let alu1 = (alu1_reg1 | alu1_neg_one) | (alu1_zero | alu1_one);
 
         let op = input.alu_op.wires;
-        let op_and = op[1 << AluOp::And as u8].expand() & (alu0 & alu1);
-        let op_or = op[1 << AluOp::Or as u8].expand() & (alu0 | alu1);
-        let op_xor = op[1 << AluOp::Xor as u8].expand() & (alu0 ^ alu1);
-        let op_add = op[1 << AluOp::Add as u8].expand() & (add_naive(alu0, alu1).sum);
+        let op_and = op[AluOp::And as usize].expand() & (alu0 & alu1);
+        let op_or = op[AluOp::Or as usize].expand() & (alu0 | alu1);
+        let op_xor = op[AluOp::Xor as usize].expand() & (alu0 ^ alu1);
+        let op_add = op[AluOp::Add as usize].expand() & (add_naive(alu0, alu1).sum);
         let alu_out = (op_and | op_or) | (op_xor | op_add);
 
         CpuAluOutput { alu_out }
