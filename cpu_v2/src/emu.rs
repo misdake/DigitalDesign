8a18570a09f658d526ac6e0bb62ce558d4055176
misdake <misdake@gmail.com>
2023-12-18 22:44:04 +0800

    (wip) emu interface.

--- cpu_v2/src/emu.rs
+++ cpu_v2/src/emu.rs
@@ -1,5 +1,11 @@
+use crate::isa::Instruction;
+
+pub struct EmuEnv {
+    inst: Box<[Instruction; 65536]>,
+    state: EmuState,
+}
+
 struct EmuState {
-    inst: Box<[u16; 65536]>,
     reg: [u16; 16],
     mem: Box<[u16; 65536]>,
 
@@ -7,4 +13,23 @@ struct EmuState {
     stack_ptr: u16,
     flag_s: u8,
     flag_z: u8,
+}
+
+// pub fn export_emu_state(&CpuState) -> EmuState { ... }
+
+// impl EmuState {
+//     pub fn diff(a: &EmuState, b: &EmuState) -> String { ...??? }
+// }
+
+impl Default for EmuState {
+    fn default() -> Self {
+        Self {
+            reg: [0; 16],
+            mem: Box::new([0; 65536]),
+            pc: 0,
+            stack_ptr: 0,
+            flag_s: 0,
+            flag_z: 0,
+        }
+    }
 }
