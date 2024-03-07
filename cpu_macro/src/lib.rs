9bf8a74978448b02610f14adb6bbdfd1ee95ce60
misdake <misdake@gmail.com>
2024-03-08 00:00:03 +0800

    more macro.

--- cpu_macro/src/lib.rs
+++ cpu_macro/src/lib.rs
@@ -10,35 +10,17 @@ pub fn make_answer(item: TokenStream) -> TokenStream {
     "fn answer() -> u32 { 42 }".parse().unwrap()
 }
 
-// pub enum IsaX {
-//     Add(Reg, Reg, Reg), // reg2, reg1, reg0 TODO InstDesc::enum_item
+// pub enum Isa {
+//     ...
 // }
-// impl IsaX {
-//     pub fn encode(&self) -> u16 {
-//         match self {
-//             IsaX::Add(reg2, reg1, reg0) => {
-//                 part3(0b0000) | part2(reg2) | part1(reg1) | part0(reg0) TODO!
-//             }
-//         }
-//     }
-//     pub fn parse(inst: InstBinaryType) -> Self {
-//         if part3(inst) == 0b0000 { return add(part2(inst), part1(inst), part0(inst)); } TODO InstDesc::parse
-//
-//         unreachable!()
-//     }
+// impl Isa {
+//     pub fn encode...
+//     pub fn parse...
 // }
-// impl Display for IsaX { TODO!
-//     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-//         match self {
-//             IsaX::Add(reg2, reg1, reg0) => {
-//                 write!(f, "r{reg0} = r{reg1} + r{reg2}")
-//             }
-//         }
-//     }
+// impl Display for Isa {
+//     fn fmt...
 // }
-// pub fn add(reg2: Reg, reg1: Reg, reg0: Reg) -> IsaX { TODO!
-//     IsaX::Add(reg2, reg1, reg0)
-// }
+// pub fn add(reg2: Reg, reg1: Reg, reg0: Reg) -> Isa { Isa::add(reg2, reg1, reg0) } TODO constructor
 
 // define_isa!{
 //     (add, 0b0000, RRR, "r{0} = r{1} + r{2}"),
@@ -56,11 +38,85 @@ fn parse_u16(s: &str) -> Result<u16, std::num::ParseIntError> {
     }
 }
 
+#[derive(Debug)]
 struct IsaDesc {
     isa_name: String,
     insts: Vec<InstDesc>,
 }
-// TODO isa fn
+impl IsaDesc {
+    pub fn generate(&self) -> String {
+        let name = self.isa_name.as_str();
+        let enum_decl = self.enum_decl();
+        let encode = self.encode();
+        let parse = self.parse();
+        format!("{enum_decl}\n\nimpl {name} {{\n{encode}\n{parse}\n}}",)
+    }
+
+    fn enum_decl(&self) -> String {
+        //pub enum Isa {
+        //    add(Reg, Reg, Reg),
+        //}
+        format!(
+            "pub enum Isa {{\n{}\n}}",
+            self.insts
+                .iter()
+                .map(|i| i.enum_item())
+                .collect::<Vec<_>>()
+                .join(",\n    ")
+        )
+    }
+    fn encode(&self) -> String {
+        //    pub fn encode(&self) -> u16 {
+        //        use Self::*;
+        //        match self {
+        //            add(reg2, reg1, reg0) => { (0b0000 << 12) | (reg2 << 8) | (reg1 << 4) | (reg0 << 0) },
+        //        }
+        //    }
+        format!(
+            "    pub fn encode(&self) -> u16 {{\n        use Self::*;\n        match self {{\n            {}\n        }}\n    }}",
+            self.insts
+                .iter()
+                .map(|i| i.encode_item())
+                .collect::<Vec<_>>()
+                .join(",\n            ")
+        )
+    }
+    fn parse(&self) -> String {
+        //    pub fn parse(inst: InstBinaryType) -> Self {
+        //        use Self::*;
+        //        if part3(inst) == 0b0000 { return add(part2(inst), part1(inst), part0(inst)); }
+        //        unreachable!()
+        //    }
+        format!(
+            "    pub fn parse(inst: InstBinaryType) -> Self {{\n        use Self::*;\n        {}\n        unreachable!()\n    }}",
+            self.insts
+                .iter()
+                .map(|i| i.parse())
+                .collect::<Vec<_>>()
+                .join("\n        ")
+        )
+    }
+    fn display(&self) -> String {
+        //impl Display for Isa {
+        //    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        //        use Self::*;
+        //        match self {
+        //            add(reg2, reg1, reg0) => { write!(f, "r{0} = r{1} + r{2}", reg0, reg1, reg2) }
+        //        }
+        //    }
+        //}
+        let name = self.isa_name.as_str();
+        format!(
+            "impl Display for {name} {{\n    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {{\n        use Self::*;\n        match self {{\n            {}\n        }}\n    }}\n}}",
+            self.insts
+                .iter()
+                .map(|i| i.display())
+                .collect::<Vec<_>>()
+                .join("\n            ")
+        )
+    }
+    //TODO constructor
+}
 
 #[derive(Debug)]
 struct InstDesc {
@@ -80,12 +136,42 @@ impl InstDesc {
             self.params(|p, pos| p.param_type(pos), ", ")
         )
     }
+    /// e.g. add(reg2, reg1, reg0)
+    fn match_item(&self) -> String {
+        format!(
+            "{}({})",
+            self.inst_name,
+            self.params(|p, pos| p.param_name(pos), ", ")
+        )
+    }
+    /// e.g. add(reg2, reg1, reg0) => { (0b0000 << 12) | (reg2 << 8) | (reg1 << 4) | (reg0 << 0) }
+    fn encode_item(&self) -> String {
+        let match_item = self.match_item();
+        let parts = self.params(|p, pos| p.value(pos), " | ");
+        format!("{match_item} => {{ {parts} }}")
+    }
     /// e.g. if part3(inst) == 0b0000 { return add(part2(inst), part1(inst), part0(inst)); }
     fn parse(&self) -> String {
         let check = self.params(|p, pos| p.check_op(pos), " && ");
         let parts = self.params(|p, pos| p.parse_part(pos), ", ");
         let inst_name = &self.inst_name;
         format!("if {check} {{ return {inst_name}({parts}); }}")
+    }
+    /// e.g. add(reg2, reg1, reg0) => { write!(f, "r{0} = r{1} + r{2}", reg0, reg1, reg2) }
+    fn display(&self) -> String {
+        let match_item = self.match_item();
+        let reverse = self.params_rev(|p, pos| p.param_name(pos), ", ");
+        format!(
+            "{match_item} => {{ write!(f, {}, {reverse}) }}",
+            self.format
+        )
+    }
+    /// e.g. pub fn add(reg2: Reg, reg1: Reg, reg0: Reg) -> Isa { Isa::add(reg2, reg1, reg0) }
+    fn constructor(&self, isa_name: &str) -> String {
+        let params = self.params(|p, pos| p.param_decl(pos), ", ");
+        let names = self.params(|p, pos| p.param_name(pos), ", ");
+        let inst_name = &self.inst_name;
+        format!("pub fn {inst_name}({params}) -> {isa_name} {{ {isa_name}::{inst_name}({names}) }}")
     }
 
     fn params<F: Fn(EncodingPart, usize) -> Option<String>>(
@@ -104,6 +190,23 @@ impl InstDesc {
         .collect::<Vec<_>>()
         .join(join)
     }
+
+    fn params_rev<F: Fn(EncodingPart, usize) -> Option<String>>(
+        &self,
+        f: F,
+        join: &'static str,
+    ) -> String {
+        [
+            f(self.type0, 0),
+            f(self.type1, 1),
+            f(self.type2, 2),
+            f(self.type3, 3),
+        ]
+        .into_iter()
+        .flatten()
+        .collect::<Vec<_>>()
+        .join(join)
+    }
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -115,8 +218,7 @@ enum EncodingPart {
     X,     // nothing
 }
 impl EncodingPart {
-    fn parse_macro(ty: char, op: u16, part_index: usize) -> Self {
-        let op = (op >> (part_index * 4)) as u8;
+    fn parse_macro(ty: char, op: u8) -> Self {
         match ty {
             'O' => EncodingPart::O(op),
             'R' => EncodingPart::R,
@@ -140,7 +242,7 @@ impl EncodingPart {
         match self {
             EncodingPart::O(_) => None,
             EncodingPart::R => Some(format!("reg{pos}")),
-            EncodingPart::I => Some("imm4".to_string()),
+            EncodingPart::I => Some(format!("imm{pos}")),
             EncodingPart::J => Some("jflags".to_string()),
             EncodingPart::X => None,
         }
@@ -158,7 +260,7 @@ impl EncodingPart {
         match self {
             EncodingPart::O(_) => None,
             EncodingPart::R => Some(format!("reg{pos}: Reg")),
-            EncodingPart::I => Some("imm4: Imm4".to_string()),
+            EncodingPart::I => Some(format!("imm{pos}: Imm4")),
             EncodingPart::J => Some("jflags: Flag4".to_string()),
             EncodingPart::X => None,
         }
@@ -170,12 +272,23 @@ impl EncodingPart {
             None
         }
     }
+    fn value(self, pos: usize) -> Option<String> {
+        match self {
+            EncodingPart::O(op) => Some(format!("(0b{op:04b} << {})", pos * 4)),
+            EncodingPart::R => Some(format!("(reg{pos} << {})", pos * 4)),
+            EncodingPart::I => Some(format!("(imm{pos} << {})", pos * 4)),
+            EncodingPart::J => Some(format!("(jflags << {})", pos * 4)),
+            EncodingPart::X => None,
+        }
+    }
 }
 
-fn parse_inst_descs(insts: TokenStream) -> Vec<InstDesc> {
-    //TODO read isa name
-    insts
-        .into_iter()
+fn parse_isa_desc(insts: TokenStream) -> IsaDesc {
+    let mut iter = insts.into_iter();
+
+    let isa_name = iter.next();
+
+    let inst_desc = iter
         .map(|inst| {
             println!("{}", inst);
             if let TokenTree::Group(inst) = inst {
@@ -201,28 +314,47 @@ fn parse_inst_descs(insts: TokenStream) -> Vec<InstDesc> {
                 );
                 let types = encoding.chars().collect::<Vec<_>>();
 
+                let op_count = types.iter().filter(|&&ty| ty == 'O').count();
+                // count = 1 -> offset =  0, 0, 0, 0
+                // count = 2 -> offset =  4, 0, 0, 0
+                // count = 3 -> offset =  8, 4, 0, 0
+                // count = 4 -> offset = 12, 8, 4, 0
+                let op_offset3 = op_count.saturating_sub(1) * 4;
+                let op_offset2 = op_count.saturating_sub(2) * 4;
+                let op_offset1 = op_count.saturating_sub(3) * 4;
+                let op_offset0 = op_count.saturating_sub(4) * 4;
+                let op3 = ((op >> op_offset3) & 0b1111) as u8;
+                let op2 = ((op >> op_offset2) & 0b1111) as u8;
+                let op1 = ((op >> op_offset1) & 0b1111) as u8;
+                let op0 = ((op >> op_offset0) & 0b1111) as u8;
+
                 InstDesc {
                     inst_name: name,
-                    type3: EncodingPart::parse_macro(types[0], op, 3), // types is left to right
-                    type2: EncodingPart::parse_macro(types[1], op, 2),
-                    type1: EncodingPart::parse_macro(types[2], op, 1),
-                    type0: EncodingPart::parse_macro(types[3], op, 0),
+                    type3: EncodingPart::parse_macro(types[0], op3),
+                    type2: EncodingPart::parse_macro(types[1], op2),
+                    type1: EncodingPart::parse_macro(types[2], op1),
+                    type0: EncodingPart::parse_macro(types[3], op0),
                     format: to_string,
                 }
             } else {
                 unreachable!("each isa inst should be token group: (name op encoding to_string)")
             }
         })
-        .collect::<Vec<_>>()
+        .collect::<Vec<_>>();
+
+    IsaDesc {
+        isa_name: isa_name.unwrap().to_string(),
+        insts: inst_desc,
+    }
 }
 
 #[proc_macro]
 pub fn define_isa(insts: TokenStream) -> TokenStream {
-    let inst_descs = parse_inst_descs(insts);
+    let isa_desc = parse_isa_desc(insts);
 
-    for inst_desc in inst_descs {
-        println!("{inst_desc:?}");
-    }
+    println!("{isa_desc:?}");
+
+    println!("{}", isa_desc.generate());
 
     "fn answer() -> u32 { 42 }".parse().unwrap()
 }
--- cpu_v2/src/isa_macro.rs
+++ cpu_v2/src/isa_macro.rs
@@ -1,6 +1,60 @@
 use cpu_macro::define_isa;
+use crate::isa::InstBinaryType;
 
-define_isa! {
-    (mov 0b0001 ORRR "r{0} = r{1}")
-    (add 0b0001 ORRR "r{0} = r{1} + r{2}")
+// define_isa! {
+//     Isa
+//     (mov 0b0001 ORRR "r{0} = r{1}")
+//     (add 0b0001 ORRR "r{0} = r{1} + r{2}")
+// }
+
+//TODO ?
+type Op4 = u8;
+type Op8 = u8;
+type Reg = u8;
+type Flag = u8;
+type Imm4 = u8;
+type Flag4 = u8;
+type Imm8Lo = u8;
+type Imm8Hi = u8;
+
+#[allow(non_camel_case_types)] //TODO allow
+pub enum Isa {
+    mov(Reg, Reg, Reg),
+    add(Reg, Reg, Reg),
+}
+
+impl Isa {
+    pub fn encode(&self) -> u16 {
+        use Isa::*; //TODO Isa
+        match self {
+            mov(reg2, reg1, reg0) => (0b0001 as u16 << 12) | (reg2 << 8) | (reg1 << 4) | (reg0 << 0), //TODO as u16
+            add(reg2, reg1, reg0) => (0b0001 << 12) | (reg2 << 8) | (reg1 << 4) | (reg0 << 0),
+        }
+    }
+    pub fn parse(inst: InstBinaryType) -> Self {
+        use Isa::*; //TODO Isa
+        if part3(inst) == 0b0001 {
+            return mov(part2(inst), part1(inst), part0(inst));
+        }
+        if part3(inst) == 0b0001 { //TODO (inst)
+            return add(part2(inst), part1(inst), part0(inst));
+        }
+        unreachable!()
+    }
+    //TODO display
+}
+//TODO constructor
+
+//TODO fn
+fn part3(binary: InstBinaryType) -> u8 {
+    ((binary >> 12) & 0b1111) as u8
+}
+fn part2(binary: InstBinaryType) -> u8 {
+    ((binary >> 8) & 0b1111) as u8
+}
+fn part1(binary: InstBinaryType) -> u8 {
+    ((binary >> 4) & 0b1111) as u8
+}
+fn part0(binary: InstBinaryType) -> u8 {
+    (binary & 0b1111) as u8
 }
