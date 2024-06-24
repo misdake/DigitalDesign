4e69a1e550b373954ca15ff1d0e9e7dee16cabd0
misdake <misdake@gmail.com>
2024-06-24 22:44:16 +0800

    change malloc header/footer. basic free.

--- cpu_v2/src/programmer/language/builtin/memory.rs
+++ cpu_v2/src/programmer/language/builtin/memory.rs
@@ -2,11 +2,10 @@ use crate::dsl::*;
 use crate::*;
 use once_cell::sync::Lazy;
 
-// size is outer size, including 2-word header
-define_struct!(Block { size, is_free });
-
-const HEAP_ROOT: u16 = 0x1000;
+const HEAP_BEGIN: u16 = 0x1000;
 const HEAP_SIZE: u16 = 0x1000;
+const HEAP_END: u16 = HEAP_BEGIN + HEAP_SIZE;
+const FREE_BIT: u16 = 1 << 15;
 
 static INIT_HEAP: Lazy<DslFunction<0, 0>> = Lazy::new(|| DslFunction::new("init_heap", [], []));
 static MALLOC: Lazy<DslFunction<1, 1>> =
@@ -21,63 +20,60 @@ pub fn define_memory(compiler: &mut Compiler) {
 
 fn define_init_heap() -> VariableOperation1 {
     INIT_HEAP.define(|[], ret| {
-        let root = v(HEAP_ROOT);
-        let root_ptr = Block::new(DslPtr::new(root));
-        let root_value = BlockValue {
-            size: v(HEAP_SIZE) - Block::SIZE as u16,
-            is_free: v(1),
-        };
-        root_ptr.write(root_value);
+        let flag = v(HEAP_SIZE | FREE_BIT);
+        DslPtr::new(v(HEAP_BEGIN)).write(flag);
+        DslPtr::new(v(HEAP_END - 1)).write(flag);
 
         ret([]);
     })
 }
 
+/// each block is header(1 word), content("size" words), footer(1 word)
+/// header = footer, bit 15: in-use flag, bit 14-0: size
 fn define_malloc() -> VariableOperation1 {
     MALLOC.define(|[mut size], ret| {
-        println!(".");
-        size += 2;
-        let end = v(HEAP_ROOT + HEAP_SIZE);
-        let ptr_var = v(HEAP_ROOT);
-        let block_ptr = DslPtr::new(ptr_var);
-        let block = Block::new(block_ptr);
+        size += 2; // include header footer
+        let free_bit = v(FREE_BIT);
+        let end = v(HEAP_END);
+        let ptr_var = v(HEAP_BEGIN);
+        let ptr = DslPtr::new(ptr_var);
 
         // while not end of heap
         while_loop(CondOp::Cmp(ptr_var, end, Cond::Less), || {
-            let block_value = block.read();
-            // if block.size >= size
+            let header = ptr.read();
+            let is_free_bit = header & free_bit;
+            let block_size = header ^ is_free_bit;
+            let free_mask = is_free_bit.asr(15);
+            // free_mask == is_free ? 0xffff : 0x0000
+
+            // if is_free && block.size >= size
             if_then(
-                CondOp::Cmp(block_value.size, size, Cond::GreaterEqual),
+                CondOp::Cmp(block_size & free_mask, size, Cond::GreaterEqual),
                 || {
                     // found it
                     // check whether next block exists
-                    let next_block_size = block_value.size - size;
+                    let next_block_size = block_size - size;
                     if_then_else(
                         CondOp::CmpI(next_block_size, 0, Cond::Greater),
                         || {
                             // next block exists, write next block
-                            let next_block_ptr = block_ptr + size;
-                            let next_block = Block::new(next_block_ptr);
-                            next_block.write(BlockValue {
-                                size: next_block_size,
-                                is_free: v(1),
-                            });
+                            let next_block_ptr = ptr + size;
+                            next_block_ptr.write(next_block_size | free_bit);
                         },
                         || {
-                            // next block does not exist, set size = block_value.size
-                            size.assign_from(block_value.size);
+                            // next block does not exist, set size = block_size
+                            size.assign_from(block_size);
                         },
                     );
-
                     // write this block
-                    block.is_free.write(v(0));
-                    block.size.write(size);
+                    ptr.write(size);
+                    (ptr + (size - 1)).write(size);
 
-                    ret([ptr_var + 2]);
+                    ret([ptr_var + 1]);
                 },
             );
 
-            block_ptr.add_var(block_value.size);
+            ptr.add_var(block_size);
         });
 
         // reached end of heap, halt
@@ -85,7 +81,19 @@ fn define_malloc() -> VariableOperation1 {
     })
 }
 fn define_free() -> VariableOperation1 {
-    todo!()
+    FREE.define(|[mut ptr], ret| {
+        ptr -= 1;
+        let ptr = DslPtr::new(ptr);
+        let size = ptr.read();
+        let flag = size | v(FREE_BIT);
+        ptr.write(flag);
+        (ptr + (size - 1)).write(flag);
+
+        //TODO try merge left
+        //TODO try merge right
+
+        ret([]);
+    })
 }
 
 pub fn init_heap() {
@@ -108,14 +116,15 @@ fn test_malloc() {
     let test_malloc = DslFunction::new("test_malloc", [], []);
     test_malloc.compile(&mut compiler, |[], _ret| {
         init_heap();
-        let _ptr1 = malloc(v(1));
+        let ptr1 = malloc(v(1));
         let _ptr2 = malloc(v(2));
         let _ptr3 = malloc(v(3));
+        free(ptr1);
         halt_with_signal(v(0));
     });
 
     let instructions = compiler.finish("test_malloc");
     let (state, _halt_signal) = simulate(&instructions, 1000);
-    let heap_mem_slice = &state.mem[HEAP_ROOT as usize..(HEAP_ROOT as usize + 20)];
+    let heap_mem_slice = &state.mem[HEAP_BEGIN as usize..(HEAP_BEGIN as usize + 20)];
     println!("heap mem: {:?}", heap_mem_slice)
 }
